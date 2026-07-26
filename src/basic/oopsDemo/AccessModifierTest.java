1050ab04a893ff829de143372af9e759dce869d8
Aditya Anand <contactaditechdev@gmail.com>
2026-07-26 13:43:48 +0530

    :fire: Remove outdated documentation files

--- src/basic/oopsDemo/AccessModifierTest.java
+++
@@ -1,9 +0,0 @@
-package basic.oopsDemo;
-
-public class AccessModifierTest {
-    // is a test
-    public static void main(String[] args) {
-
-        Dog d  = new Dog();
-    }
-}
--- src/basic/oopsDemo/oopsDogs/animalDemo/Dogeee.java
+++
@@ -1,9 +0,0 @@
-package basic.oopsDemo.oopsDogs.animalDemo;
-
-public class Dogeee extends Animal{
-//
-    Dogeee(String name, boolean isHungry){
-        super(name, isHungry);
-    }
-
-}
